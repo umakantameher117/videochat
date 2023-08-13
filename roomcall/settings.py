0cf65fdce9da9b78bbe9e9bec2f4d624ea0d8e4d
umakantameher117 <umakantameher555@gmail.com>
2023-08-13 23:02:38 +0530

    vercel ready

--- roomcall/settings.py
+++ roomcall/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-0bxtr-)#sln&x4@^jqk@-$v$u8qd2k1uwg3y$*!4&y9byg5v^6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app','.now.sh']
 
 
 # Application definition
@@ -121,6 +121,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles_build','static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
